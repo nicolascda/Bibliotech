7a899f42d713e8b01ff29d1e6ad2e830f172154c
NICOLAS CLAUDINO DE AGUIAR <25162441@edu119.local>
2025-11-28 16:46:49 -0300

    Atualização do admin

---
+++ JS/admin.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("livrosCount").innerText = 100;
+    document.getElementById("usuariosCount").innerText = 87;
+    document.getElementById("emprestadosCount").innerText = 45;
+});
